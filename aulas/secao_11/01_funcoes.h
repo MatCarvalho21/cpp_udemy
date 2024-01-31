d062075346cc1c4c369c475c212dadab7dd9ec0e
Matheus Carvalho <matheuscarvalho210404@outlook.com>
2024-01-31 08:52:51 -0300

    dividindo o projeto em módulos

---
+++ aulas/secao_11/01_funcoes.h
@@ -0,0 +1,3 @@
+void printaOI(){
+    printf("\nOI"); 
+}
---
+++ aulas/secao_11/01_main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <iostream>
+#include <string>
+#include "01_funcoes.h"
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    for(int i = 0; i < 10; i++){
+        printaOI();
+    }
+}
